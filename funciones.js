5869d73b43caea760504009b535846559a6de25e
Mateo Machado <mateo.machado@masglobalconsulting.com>
2023-08-25 09:33:41 -0500

    formulario con funciones JS

--- funciones.js
+++ funciones.js
@@ -1,55 +1,87 @@
-//variables globales
-const d = document;
-let campoTexto = d.querySelector(".texto");
-let botonCrear = d.querySelector(".boton");
-let tabla = d.querySelector(".table tbody");
 
 
-botonCrear.addEventListener("click",function(){
-    crearTarea();
-});
-let contar = 1;
-function crearTarea() {
-    if(campoTexto.value == ""){
-        alert("Ingresa una tarea");
-        return;
-    }
-    let fila = d.createElement("tr");
-    fila.innerHTML =` <td> ${contar++} </td>
-    <td> <span class="respuesta"> ${campoTexto.value} </span> </td>
-    <td> <span class="botonEditar" onclick="editarTarea(this)"> ✍ </span> </td>
-    <td> <span class="botonEliminar" onclick="eliminarTarea()"> ❌ </span> </td>`;
-    tabla.appendChild(fila);
-    campoTexto.value = "";
-}
-function eliminarTarea(){
-    let botonEliminar = d.querySelector(".botonEliminar");
-    let confirmar = confirm("¿desea eliminar esta tarea?");
-    if(confirmar){
-        //alert("El usuario elimino");
-        botonEliminar.parentElement.parentElement.remove();
-    }
-}
-campoTexto.addEventListener("keyup",function(e){
-    //console.log(e.key);
-    if( e.key == "Enter"){
-        crearTarea();
-    }
-})
-function editarTarea(icon){
-    let encontrarfila =icon.closest("tr");
-    let asignar = encontrarfila.querySelector(".respuesta");
-    let nuevotexto = prompt("ingrese edicion")
 
-    if(nuevotexto !== null){
-        asignar.textContent = nuevotexto;
-    }
+   /* let usuario = [{
+    nombre : "juan",
+    cargo : "frontend",
+    salario : 4500000
+  },
+ {nombre : "mateo",
+  cargo : "developer",
+  salario : 4899999
+}]*/
+
+  //necesitamos JSON para la intercomunicacion de la base de datos y apis 
+  // y tiene dos metodos uno llamado json.parse(convierte de texto a estado original)
+  // y json.stringfy(lo convierte en texto)
+  //guardar datos en el navegador
+  ///localStorage.setItem("datos", JSON.stringify(usuario));
+ 
+  
     
-    
-    
-    
-}
+  
+  /*let datos =JSON.parse(localStorage.getItem("datos"))
+  document.write("nombre : "+datos.nombre+"<br>")
+  document.write("cargo : "+datos.cargo+"<br>")
+  document.write("salario : "+datos.salario+"<br>")*/   
 
 
-//let respuesta = d.querySelector(".respuesta");
-  //  respuesta.textContent = prompt("ingrese la edicion")
+  //variables globales
+  const d = document;
+  let nombre = d.querySelector(".nombre-pro");
+  let presentacion = d.querySelector(".presentacion-pro");
+  let precio = d.querySelector(".precio-pro");
+  let imagen= d.querySelector(".imagen-pro")
+  let btnguardar = d.querySelector(".btn-guardar");
+
+  //agregar evento al boton
+  btnguardar.addEventListener("click", function(){
+    obtenerDatos();
+   /* alert("nombre: "+ nombre.value + "\n"
+    +"presentacion: "+ presentacion.value+"\n"
+    +"precio: "+precio.value+"\n"
+    +"urlimagen: "+ imagen.value+"\n"
+    );*/
+  })
+
+
+  //funcion para obtener los datos del formulario
+  function obtenerDatos(){
+    if (nombre.value == ""||
+        presentacion.value == ""||
+        precio.value == "" ||
+        imagen.value == "" ) {
+            alert("debes escribir un campo");
+            return;
+        
+    }
+    let datos = {
+        "nombre": nombre.value,
+        "presentacion": presentacion.value,
+        "precio": precio.value,
+        "imagen": imagen.value
+    }
+    nombre.value = "";
+    presentacion.value = "";
+    precio.value = "";
+    imagen.value = "";
+
+    console.log(datos)
+    guardarDatos(datos)
+  }
+  let productosKey = "productos"
+  //funcion para guardar los datos en localstorage
+  function guardarDatos( objeto ){
+    let productos = [];
+    let proGuardadosEnLocal = JSON.parse( localStorage.getItem(productosKey));
+    if (proGuardadosEnLocal != null) {
+        productos = proGuardadosEnLocal;
+        
+    }
+    //guardar producto nuevo
+
+    productos.push(objeto);
+    //guardar local
+    localStorage.setItem(productosKey, JSON.stringify(productos));
+    alert("productos guardados con exito");
+  }
